2a8b903ef03a5f78673212de500d470e1f5a6eb7
PreQursor <preqursor.team@gmail.com>
2025-04-03 01:30:37 +0500

    PQ logo update...:)

--- src/components/LegalHeader.tsx
+++ src/components/LegalHeader.tsx
@@ -2,7 +2,7 @@ const LegalHeader: React.FC = () => {
     return (
         <div>
             <div className="flex justify-center items-center py-2">
-                <img src="/assets/pq logo.jpg" alt="PQ Logo" className="h-20 w-20 rounded-full" />
+                <img src="/assets/PreQursor - Logo.png" alt="PQ Logo" className="h-24 w-24 rounded-full" />
             </div>
             <div className="font-orbitron font-bold text-3xl py-1 tracking-wide text-center text-orangered">PreQursor</div>
         </div>
